0082d314072aa2e1e81c887a472ac33f81fafead
Shawn Yu <shawn.yu5@hotmail.com>
2021-03-28 20:35:18 -0400

    email not working
    everything else but email working

--- dbs_ms1.cpp
+++ dbs_ms1.cpp
@@ -1,3 +1,5 @@
+
+
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <iostream>
@@ -24,10 +26,10 @@ struct Employee
 };
 
 //prototypes
-int const menuu();
-int findEmployee(Connection *conn, int employeeNumber, struct Employee* emp);
-int getInt(string prompt = nullptr);
-void displayEmployee(Connection* conn, struct Employee emp);
+int const menu();
+int findEmployee(Connection* conn, int employeeNumber, struct Employee* emp);
+int getInt(const char* prompt = nullptr);
+void displayEmployee(Connection* conn, struct Employee* emp);
 
 
 int main(void) {
@@ -37,22 +39,22 @@ int main(void) {
 
     // User Variables
     string user = "dbs211_211k20";
-    string pass = "xxxxxxx";
+    string pass = "36932186";
     string constr = "myoracle12c.senecacollege.ca:1521/oracle12c";
 
-    try{
+    try {
         env = Environment::createEnvironment(Environment::DEFAULT);
         conn = env->createConnection(user, pass, constr);
         cout << "Connection is Successful!" << endl;
 
         bool flag = false;
         do {
-            int menu = menuu();
-            if (menu == 0) {
+            int menuSelection = menu();
+            if (menuSelection == 0) {
                 cout << "Ending program... " << endl;
                 flag = true;
             }
-            else if (menu == 1) {
+            else if (menuSelection == 1) {
                 // Find Employee
                 struct Employee emp;
                 int employeeNum = getInt("Enter employee number: ");
@@ -60,37 +62,38 @@ int main(void) {
                 int tmp = findEmployee(conn, employeeNum, &emp);
                 if (tmp) {
                     //call display function here
+                    displayEmployee(conn, &emp);
                 }
                 else {
                     cout << "Employee " << employeeNum << " does not exist" << endl;
                 }
             }
-            else if (menu == 2) {
-            cout << "function not yet available" << endl;   
+            else if (menuSelection == 2) {
+                cout << "function not yet available" << endl;
             }
-            else if (menu == 3) {
-            cout << "function not yet available" << endl;   
+            else if (menuSelection == 3) {
+                cout << "function not yet available" << endl;
             }
-            else if (menu == 4) {
-            cout << "function not yet available" << endl;
+            else if (menuSelection == 4) {
+                cout << "function not yet available" << endl;
             }
-            else if (menu == 5) {
-            cout << "function not yet available" << endl;
+            else if (menuSelection == 5) {
+                cout << "function not yet available" << endl;
             }
-        } while (!flag); 
+        } while (!flag);
 
         env->terminateConnection(conn);
         Environment::terminateEnvironment(env);
     }
-    catch (SQLException & sqlExcp) {
+    catch (SQLException& sqlExcp) {
         cout << sqlExcp.getErrorCode() << ": " << sqlExcp.getMessage();
     }
     return 0;
 }
 
 // menu function
-int const menuu()  {
-    int tmp; 
+int const menu() {
+    int tmp;
     bool flag = false;
 
     cout << "********************* HR Menu *********************" << endl;
@@ -100,42 +103,38 @@ int const menuu()  {
     cout << "4) Update Employee" << endl;
     cout << "5) Remove Employee" << endl;
     cout << "0) Exit" << endl;
-    
+
     do {
         tmp = getInt("Enter an option (0-5): ");
-        if (tmp > 5 || tmp < 0) { cout << "Invalid option, try again" << endl; }
+        if (tmp > 5 || tmp < 0)
+        {
+            cout << "Invalid option, try again" << endl;
+        }
         else { flag = true; }
     } while (!flag);
 
     return tmp;
 }
 
-int getInt(string prompt) {
+int getInt(const char* prompt) {
     int input;
     char next;
     bool flag = false;
 
-    if (prompt != "")
+    if (prompt)
     {
         cout << prompt;
     }
 
-     do {
+    do {
         cin >> input;
 
-        if (cin.fail() || input < -1 || input > 6) {
+        if (cin.fail()) {
             cerr << "Invalid option, try again" << endl;
             cin.clear();
             cin.ignore(1000, '\n');
             cout << prompt;
         }
-       /* else if (input < -1 || input > 6)
-        {
-            cerr << "Invalid option, try again" << endl;
-            cin.clear();
-            cin.ignore(1000, '\n');
-            cout << prompt;
-        }*/
         else {
             next = cin.get();
             if (next != '\n') {
@@ -144,7 +143,7 @@ int getInt(string prompt) {
             }
             else { flag = true; }
         }
-    } while(!flag);
+    } while (!flag);
 
     return input;
 }
@@ -154,44 +153,42 @@ int findEmployee(Connection* conn, int employeeNumber, struct Employee* emp) {
     int tmp = 0;
 
     try {
-       
-        Statement* stmt = conn->createStatement("SELECT e.employeenumber, e.lastname, e.firstname, e.extension, e.email, e.officecode, e.reportsTo, e.jobtitle FROM dbs211_employees e JOIN dbs211_offices o ON e.officecode = o.officecode WHERE e.employeenumber = :1");
+
+        Statement* stmt = conn->createStatement("SELECT * FROM dbs211_employees WHERE employeenumber = :1");
         stmt->setNumber(1, employeeNumber);
         ResultSet* rs = stmt->executeQuery();
-
-        if (rs->next()) {  
+        
+        if (rs->next()) {
             emp->employeeNumber = rs->getInt(1);
             strcpy(emp->lastName, rs->getString(2).c_str());
             strcpy(emp->firstName, rs->getString(3).c_str());
             strcpy(emp->extension, rs->getString(4).c_str());
-            strcpy(emp->email, rs->getString(5).c_str());
+            //strcpy(emp->email, rs->getString(5).c_str());  
             strcpy(emp->officecode, rs->getString(6).c_str());
             emp->reportsTo = rs->getInt(7);
             strcpy(emp->jobTitle, rs->getString(8).c_str());
             
-            tmp = 1;    
+            tmp = 1;
         }
         conn->terminateStatement(stmt);
         return tmp;
     }
-    catch (SQLException & sqlExcp) {
+    catch (SQLException& sqlExcp) {
         cout << sqlExcp.getErrorCode() << ": " << sqlExcp.getMessage();
     }
 }
 
-void displayEmployee(Connection* conn, struct Employee emp)
+void displayEmployee(Connection* conn, struct Employee* emp)
 {
-    int empNumber;
-    cout << "please enter the employee number: "; 
-    int empNumber = getInt();
-
-    if (findEmployee(empNumber) == 0)
-    {
-        cout << "Employee " << empNumber << "does not exist" << endl; 
-    }
-    else
-    {
-        displayEmployee(/*SOMETHING*/); 
-    }
-
+    cout << "-------------- Employee Information -------------" << endl
+        << "Employee Number: " << emp->employeeNumber << endl
+        << "Last Name: " << emp->lastName << endl
+        << "First Name: " << emp->firstName << endl
+        << "Extension: " << emp->extension << endl
+       //  << "Email: " << emp->email << endl
+        << "Office Code: " << emp->officecode << endl
+        << "Manager ID :" << emp->reportsTo << endl
+        << "Job Title: " << emp->jobTitle << endl;
+        //cout << "Employee found!!" << endl;
+        //displayEmployee(conn, emp); 
 }
